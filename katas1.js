726fcb4eb3854dfbf220bcde36ca8ac7e49c5333
Lucas Batista <batista.lucas3098@gmail.com>
2021-08-07 00:02:09 -0300

    Arrumando let

--- katas1.js
+++ katas1.js
@@ -1,129 +1,129 @@
 function oneThroughTwenty() {
-  let meuRetorno = [];
+  let contador = [];
 
   for (let counter = 1; counter <= 20; counter++) {
-      meuRetorno.push(counter);
+     contador.push(counter);
   }
 
-  return meuRetorno;
+  return contador;
  
 }
 
 console.log(oneThroughTwenty());
 
 function evensToTwenty() {
-  let meuRetorno = [];
+  let contador = [];
 
   for (let counter = 2; counter <= 20; counter += 2) {
-    meuRetorno.push(counter);
+   contador.push(counter);
 }
 
-return meuRetorno;
+return contador;
   
 }
 
 console.log(evensToTwenty());
 
 function oddsToTwenty() {
-  let meuRetorno = [];
+  let contador = [];
 
   for (let counter = 1; counter <= 20; counter += 2) {
-    meuRetorno.push(counter);
+   contador.push(counter);
 }
 
-return meuRetorno;
+return contador;
    
 }
 
 console.log(oddsToTwenty());
 
 function multiplesOfFive() {
-  let meuRetorno = [];
+  let contador = [];
 
   for (let counter = 5; counter <= 100; counter += 5) {
-    meuRetorno.push(counter);
+   contador.push(counter);
 }
 
-return meuRetorno;
+return contador;
    
 }
 
 console.log(multiplesOfFive());
 
 function squareNumbers() {
-  let meuRetorno = [];
+  let contador = [];
 
   for (let counter = 1; counter <= 10; counter++ ) {
     let num = counter * counter;
-      meuRetorno.push(num);
+     contador.push(num);
     }
 
-return meuRetorno;
+return contador;
   }
       
 console.log(squareNumbers());
 
 function countingBackwards() {
-  let meuRetorno = [];
+  let contador = [];
 
   for (let counter = 20; counter >= 1; counter--) {
-    meuRetorno.push(counter);
+   contador.push(counter);
 }
 
-return meuRetorno;
+return contador;
    
 }
 
 console.log(countingBackwards());
 
 function evenNumbersBackwards() {
-  let meuRetorno = [];
+  let contador = [];
 
   for (let counter = 20; counter >= 1; counter -= 2) {
-    meuRetorno.push(counter);
+   contador.push(counter);
 }
 
-return meuRetorno;
+return contador;
    
 }
 
 console.log(evenNumbersBackwards());
 
 function oddNumbersBackwards() {
-  let meuRetorno = [];
+  let contador = [];
 
   for (let counter = 19; counter >= 1; counter -= 2) {
-    meuRetorno.push(counter)
+   contador.push(counter)
 }
 
-return meuRetorno;
+return contador;
    
 }
 
 console.log(oddNumbersBackwards());
 
 function multiplesOfFiveBackwards() {
-  let meuRetorno = [];
+  let contador = [];
 
   for (let counter = 100; counter >= 5; counter -= 5) {
-    meuRetorno.push(counter);
+   contador.push(counter);
 }
 
-return meuRetorno;
+return contador;
    
 }
 
 console.log(multiplesOfFiveBackwards());
 
 function squareNumbersBackwards() {
-  let meuRetorno = [];
+  let contador = [];
 
   for (let counter = 10; counter >= 1; counter-- ) {
     let num = counter * counter;
-      meuRetorno.push(num);
+     contador.push(num);
     }
 
-return meuRetorno;
+return contador;
 }
 
 console.log(squareNumbersBackwards());
